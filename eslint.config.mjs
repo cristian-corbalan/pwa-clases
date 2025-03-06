6088e0f98d252385611d0417697e725c9570b557
cristian-corbalan <ccorbalandw@gmail.com>
2025-03-06 14:04:46 -0300

    First commit

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
